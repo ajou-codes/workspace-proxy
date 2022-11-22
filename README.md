ffbb1faf26cb1bcf516f9c70cae6a53576dc439a
Jin Hyeok Son <thswlsgur98@gmail.com>
2022-11-22 22:04:37 +0900

    feat: reverse proxy logic

---
+++ README.md
@@ -0,0 +1,4 @@
+# Workspace Proxy
+workspace의 pod으로 연결을 중개해주는 proxy
+
+PoC 검증을 위한 Typescript 코드로, 성능 개선을 위해 Go로 재작성 필요
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import * as k8s from "@kubernetes/client-node";
+import http from "http";
+import httpProxy from "http-proxy";
 
 const kc = new k8s.KubeConfig();
-
 kc.loadFromDefault();
 
 const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
@@ -18,11 +19,8 @@ const listFn = () => k8sApi.listNamespacedPod(
     LABEL_SELECTOR
 );
 
-const informer = k8s.makeInformer(kc, `/api/v1/${NAMESPACE_NAME}/default/pods`, listFn, LABEL_SELECTOR);
+const informer = k8s.makeInformer(kc, `/api/v1/namespaces/${NAMESPACE_NAME}/pods`, listFn, LABEL_SELECTOR);
 
-informer.on('add', (obj: k8s.V1Pod) => { console.log(`Added: ${obj.metadata!.name}`); });
-informer.on('update', (obj: k8s.V1Pod) => { console.log(`Updated: ${obj.metadata!.name}`); });
-informer.on('delete', (obj: k8s.V1Pod) => { console.log(`Deleted: ${obj.metadata!.name}`); });
 informer.on('error', (err: k8s.V1Pod) => {
     console.error(err);
     // Restart informer after 5sec
@@ -32,14 +30,53 @@ informer.on('error', (err: k8s.V1Pod) => {
 });
 
 informer.start().then(() => {
-    const loopDemo = () => {
-        console.log(informer.list().length);
-        for (let v1Pod of informer.list()) {
-        }
-
-        setTimeout(loopDemo, 2000);
+    function getWorkspaceInfo(workspaceId): k8s.V1Pod {
+        return informer.get(workspaceId);
     }
 
-    loopDemo();
+    const proxy = httpProxy.createProxyServer({ ws: true, secure: false, changeOrigin: true });
+
+    const server = http.createServer((req, res) => {
+        // node.js http library make headers all lowercase
+        const workspaceId = req.headers['x-workspace-id'];
+
+        if (!workspaceId){
+            res.statusCode = 404;
+            res.end();
+            return;
+        }
+
+        // WIP: for port forwarding
+        const port = req.headers['x-workspace-port'];
+
+
+        // find pod ip
+        const workspaceInfo = getWorkspaceInfo(workspaceId);
+
+        if (!workspaceInfo){
+            res.statusCode = 404;
+            res.end();
+            return;
+        }
+
+        const workspaceUrl = `http://${workspaceInfo.status.podIP}.${NAMESPACE_NAME}.pod.cluster.local`;
+
+        proxy.web(req, res, {
+            target: workspaceUrl,
+        });
+    });
+
+    // Web Socket
+    server.on('upgrade', (req, socket, head) => {
+        const workspaceId = req.headers['x-workspace-id'];
+        const workspaceInfo = getWorkspaceInfo(workspaceId);
+        const workspaceUrl = `http://${workspaceInfo.status.podIP}.${NAMESPACE_NAME}.pod.cluster.local`;
+
+        proxy.ws(req, socket, head, {
+            target: workspaceUrl
+        });
+    });
+
+    server.listen(8000);
 });
 
